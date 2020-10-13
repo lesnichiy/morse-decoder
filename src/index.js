9ce6ad90f1d320485bab74ddfd4505bf452662b8
lesnichiy <lesnichiy02@ya.ru>
2020-10-14 02:06:38 +0300

    feat: add a solution to morse-decoder task

--- src/index.js
+++ src/index.js
@@ -39,8 +39,42 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+  const exprArr = [];
+
+  for (let i = 0; i < expr.length; i += 10) {
+    exprArr.push(expr.substring(i, i + 10));
+  }
+
+  let morseDecodedStr = ``;
+
+  for (let i = 0; i < exprArr.length; i++) {
+    if (exprArr[i] === '**********') {
+      morseDecodedStr = `${morseDecodedStr} `;
+    } else {
+      let morseSymbolArr = [];
+
+      for (let j = 0; j < exprArr[i].length; j += 2) {
+        morseSymbolArr.push(exprArr[i].substring(j, j + 2));
+      }
+
+      let currentLetter = morseSymbolArr.reduce( (str, item) => {
+        if (item === '10') {
+          return `${str}.`;
+        } else if (item === '11') {
+          return `${str}-`;
+        } else {
+          return `${str}`;
+        }
+      }, ``);
+
+      morseDecodedStr = `${morseDecodedStr}${MORSE_TABLE[currentLetter]}`
+
+    }
+  }
+
+  return morseDecodedStr;
 }
 
 module.exports = {
     decode
-}
+};
